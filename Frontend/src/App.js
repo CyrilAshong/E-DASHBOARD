031c4cc877f24935e53e5b5e782a118577cad3a9
AshongCyril <cashong005@st.ug.edu.gh>
2024-05-29 13:16:21 +0000

    Added an outlet to return the sign up page when there is no user stored in the local storage and return the other pages when a user is registered

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import  Nav  from "./components/Nav";
 import Footer from "./components/Footer"
 import SignUp from './components/SignUp';
+import PrivateComponent from './components/PrivateComponent';
 import { React } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -11,11 +12,13 @@ function App() {
       <BrowserRouter>
       <Nav />
       <Routes>
+        <Route element={<PrivateComponent />}>
         <Route path='/' element={<h1>Product Component</h1>}></Route>
         <Route path='/add' element={<h1>Add Product Component</h1>}></Route>
         <Route path='/update' element={<h1>Update Product Component</h1>}></Route>
         <Route path='/logout' element={<h1>Logout Component</h1>}></Route>
         <Route path='/profile' element={<h1>Profile component</h1>}></Route>
+        </Route>
         <Route path='/signup' element={<SignUp />}></Route>
       </Routes>
       </BrowserRouter>
